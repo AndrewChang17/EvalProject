83f9102aa3fe4bd5e25dd4e6f40ec77625236a55
Chaos <andrewchang17@hotmail.com>
2018-11-06 11:59:34 -0800

    centerMapOnSite, sidebar complete

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -27,7 +27,7 @@ class AccordionButton extends Component {
     SubButtons = (sites) => {
         return (
             sites.map(site => (
-                <button className={styles.accordion} onClick={this.props.onClickSubitem} id={site.id}>
+                <button className={styles.accordion} onClick={() => this.props.onClickSubitem(site.id)}>
                     {site.name}
                 </button>
             ))
